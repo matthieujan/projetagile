668ac81139504d61357bf06820533c8000512137
francois76 <fgognet@gmail.com>
2018-01-08 18:10:52 +0100

    premier round

---
+++ src/main/java/sparkexample/Routes.java
@@ -0,0 +1,49 @@
+package sparkexample;
+
+import static spark.Spark.*;
+
+public class Routes {
+	
+
+    public static void setRoutes() {
+    	
+        get("/", (request, response) -> {
+        	response.status(200);          
+        	response.type("text/xml");
+        	String documentation = "Doc : \n";
+        	documentation += "/demandes: pas implémenté\n";
+        	documentation += "/demandes/id: pas implémenté\n";
+        	return documentation;
+        });
+        
+        post("/demandes", (request, response) -> {
+        	response.status(501);          
+        	response.type("text/xml");  
+        	return "\"demandes/\" pas implémenté ";
+        });
+        
+        put("/demandes/:id", (request, response) -> {
+        	response.status(501);          
+        	response.type("text/xml");  
+        	return "\"demandes/id\" pas implémenté ";
+        });
+        
+        get("/demandes", (request, response) -> {
+        	response.status(501);          
+        	response.type("text/xml");  
+        	return "\"demandes/\" pas implémenté ";
+        });
+        
+        get("/demandes/:id", (request, response) -> {
+        	response.status(501);          
+        	response.type("text/xml");  
+        	return "\"demandes/id\" pas implémenté ";
+        });
+        
+        post("/demandes/:id", (request, response) -> {
+        	response.status(501);          
+        	response.type("text/xml");  
+        	return "\"demandes/id\" pas implémenté ";
+        });
+    }
+}
