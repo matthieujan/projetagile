65f52785855397b78f1d9821bde3e4c8fda67a15
FelixBez <f.bezancon@gmail.com>
2018-01-15 17:42:53 +0100

    PostDemandIdRoute

--- src/main/java/sparkexample/PostDemandIdRoute.java
+++ src/main/java/sparkexample/PostDemandIdRoute.java
@@ -3,12 +3,22 @@ package sparkexample;
 import spark.Request;
 import spark.Response;
 
+
+//VOTER
 public class PostDemandIdRoute {
 	
 	public static String handle(Request request, Response response) {
-		response.status(501);
+		response.status(200);
     	response.type("text/html; charset=utf-8");
-    	return "\"demandes/id\" pas implémenté ";
+    	
+    	String demande = Demandes.getDemandesById(Integer.parseInt(request.params(":id")));
+    	Demandes.removeDemandesById(Integer.parseInt(request.params(":id")));
+    	if(request.body().equals("oui")) {
+    		demande = "La requete "+demande+" est validé.";
+    	}else {
+    		demande = "La requete "+demande+" est refusé.";
+    	}
+    	return demande;
 	}
 
 
