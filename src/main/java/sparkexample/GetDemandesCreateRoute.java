e1c0060df8662945069ea86cf3b4622b871f2959
LeGrandMaxime <rischebourg.e1601054@etud.univ-ubs.fr>
2018-01-26 16:30:00 +0100

    choix

---
+++ src/main/java/sparkexample/GetDemandesCreateRoute.java
@@ -0,0 +1,38 @@
+package sparkexample;
+
+import spark.Request;
+import spark.Response;
+
+public class GetDemandesCreateRoute {
+
+	
+	
+	//créer une demande
+	
+	public static String handle(Request request, Response response) {
+		response.status(200);
+		response.type("text/html; charset=utf-8");
+		String doc = "";
+		doc += " <html>";
+		doc += " <body>";
+		doc += " <p>============Crée une demande =================== <br /></p>";
+
+		doc +="<form action=\"/demandes\" method=\"post\">";
+		doc +="name:<br>";
+		doc +="<input type=\"text\" name=\"description\" ><br>";
+		doc +="<input type=\"text\" name=\"chargeutile\" ><br>";
+		doc +="<input type=\"text\" name=\"choix1\" ><br>";
+		doc +="<input type=\"text\" name=\"url1\" ><br>";
+		doc +="<input type=\"text\" name=\"choix2\" ><br>";
+		doc +="<input type=\"text\" name=\"url2\" ><br>";
+		doc +="<input type=\"submit\" value=\"Submit\">";
+		doc +="</form>";
+
+		doc += " </body>";
+		doc += " </html>";
+
+
+		return doc;
+	}
+
+}
